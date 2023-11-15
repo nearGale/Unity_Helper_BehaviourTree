3e147c1906b2332991849aa99e8a7546974b9c96
重剑无锋 <xuuold@gmail.com>
2023-11-15 18:41:31 +0800

    增加反向装饰器节点

---
+++ BehaviourTree/Assets/BehaviorTree/Nodes/DecoratorNodes/DecoratorInvert.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+
+    public class DecoratorInvert : Decorator
+    {
+        public DecoratorInvert(BTreeBehavior child) : base(child)
+        {
+        }
+
+        protected override BTreeStatus Update()
+        {
+            if (m_Child == null) { return BTreeStatus.Failure; }
+
+            BTreeStatus status = m_Child.Tick();
+
+            if (status == BTreeStatus.Success)
+            {
+                return BTreeStatus.Failure;
+            }
+
+            if (status == BTreeStatus.Failure)
+            {
+                return BTreeStatus.Success;
+            }
+
+            return status;
+        }
+    }
+}
